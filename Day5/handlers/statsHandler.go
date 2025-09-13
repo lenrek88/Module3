39a0e651dd610d19b7f214beeb500088ddf7cee4
lenrek88 <lenrek88@yandex.ru>
2025-09-14 00:50:56 +0300

    add stats

--- Day5/handlers/statsHandler.go
+++ Day5/handlers/statsHandler.go
@@ -1,30 +1,77 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"lenrek88/logger"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 func StatsHandler(w http.ResponseWriter, r *http.Request) {
 
-	//start := r.URL.Query().Get("start")
-	//end := r.URL.Query().Get("end")
-	//
-	//if start == "" || end == "" {
-	//	err := fmt.Errorf("StatsHandler: parameters start or end are empty")
-	//	logger.Error("RateHandler error", err)
-	//	http.Error(w, "Missing parameters: from and to required", http.StatusBadRequest)
-	//	return
-	//}
-	//
-	//data, err := os.ReadFile("./app.log")
-	//
-	//data, err := json.Marshal(rate)
+	start := r.URL.Query().Get("start")
+	end := r.URL.Query().Get("end")
+
+	timeLayout := "2006-01-02"
+
+	startDateParse, err := time.Parse(timeLayout, start)
+	if err != nil {
+		err := fmt.Errorf("StatsHandler: time parse error: %s", err.Error())
+		logger.Error("StatsHandler error", err)
+		return
+	}
+
+	endDateParse, err := time.Parse(timeLayout, end)
+	if err != nil {
+		err := fmt.Errorf("StatsHandler: time parse error: %s", err.Error())
+		logger.Error("StatsHandler error", err)
+		return
+	}
+
+	data, err := os.ReadFile("./app.log")
+	dataLines := strings.Split(string(data), "\n")
+	var amountRateContains int64
+	var amountExchangeContains int64
+	for _, line := range dataLines {
+		parts := strings.Split(line, " ")
+		if len(parts) >= 2 {
+			dateString := parts[1]
+			parsedTime, err := time.Parse("2006/01/02", dateString)
+			if err != nil {
+				err := fmt.Errorf("StatsHandler: date parse error: %s", err.Error())
+				logger.Error("StatsHandler error", err)
+				return
+			}
+			newFormat := parsedTime.Format(timeLayout)
+			timeParseNewFormat, _ := time.Parse(timeLayout, newFormat)
+			fmt.Println(timeParseNewFormat, startDateParse, endDateParse)
+			if (timeParseNewFormat.After(startDateParse) || timeParseNewFormat.Equal(startDateParse)) && (timeParseNewFormat.Before(endDateParse) || (timeParseNewFormat.Equal(endDateParse))) {
+				if strings.Contains(line, "rate") {
+					amountRateContains = amountRateContains + 1
+				}
+				if strings.Contains(line, "exchange") {
+					amountExchangeContains = amountExchangeContains + 1
+				}
+			}
+		}
+
+	}
+
+	strRate := fmt.Sprintf("%d", amountRateContains)
+	strExchange := fmt.Sprintf("%d", amountExchangeContains)
+
+	t := "на период c " + start + " по " + end + " были выполнены запросы : " + "rate : " + strRate + " exchange : " + strExchange
+
+	stats, err := json.Marshal(t)
 	//if err != nil {
 	//	err = fmt.Errorf("handler: failed to marshal response: %w", err)
 	//	logger.Error("RateHandler error", err)
 	//	http.Error(w, "Internal server error", http.StatusInternalServerError)
 	//	return
 	//}
-	//w.Write(data)
+	w.Write(stats)
 
 }
